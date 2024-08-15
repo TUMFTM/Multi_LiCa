f52f38200b0046cac64b1f0efb287331503299ad
himadriroyy <himadri@aerovect.com>
2024-08-15 14:24:36 -0400

    added updated config. launch, docker_launch script for ease of use

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -7,11 +7,14 @@ xhost +local:
 docker run -it --rm --privileged \
     --network host \
     -e DISPLAY=${DISPLAY} \
+    -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $SCRIPT_DIR/../config:/ros_ws/src/multi_lidar_calibration/config \
     -v $SCRIPT_DIR/../data:/ros_ws/src/multi_lidar_calibration/data \
     -v $SCRIPT_DIR/../output:/ros_ws/src/multi_lidar_calibration/output \
     tum.ftm.multi_lidar_calibration:latest \
-    /bin/bash
+    /bin/bash -c "source /ros_ws/install/setup.bash && \
+    ros2 launch multi_lidar_calibrator calibration.launch.py && \
+    ros2 launch multi_lidar_calibrator calibration.launch.py lidar_topics:='["/center_lidar/lidar_points", "/right_lidar/lidar_points"]'"
 
 xhost -local:
--- launch/calibration.launch.py
+++ launch/calibration.launch.py
@@ -1,8 +1,6 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
@@ -10,9 +8,10 @@ from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     pkg_share = get_package_share_directory("multi_lidar_calibrator")
-    parameter_file = os.path.join(pkg_share, "config", "demo.yaml")
+    parameter_file = os.path.join(pkg_share, "config", "params.yaml")
     output = os.path.join(pkg_share, "output")
 
+    # Declare launch arguments
     output_dir_arg = DeclareLaunchArgument(
         "output_dir", default_value=output, description="Path to the output directory."
     )
@@ -23,19 +22,33 @@ def generate_launch_description():
         description="Path to the ROS2 parameters file to use.",
     )
 
+    lidar_topics_arg = DeclareLaunchArgument(
+        "lidar_topics",
+        default_value='["/center_lidar/lidar_points", "/left_lidar/lidar_points"]',
+        description="List of LiDAR topics."
+    )
+
+    # Use LaunchConfiguration to fetch the values of the arguments
     parameter_file_launch_config = LaunchConfiguration("parameter_file")
     output_dir_launch_config = LaunchConfiguration("output_dir")
+    lidar_topics_launch_config = LaunchConfiguration("lidar_topics")
 
     return LaunchDescription(
         [
             params_declare,
             output_dir_arg,
+            lidar_topics_arg,
             Node(
                 package="multi_lidar_calibrator",
                 executable="multi_lidar_calibrator",
                 name="multi_lidar_calibration_node",
-                parameters=[parameter_file_launch_config,
-                            output_dir_launch_config],
+                parameters=[
+                    parameter_file_launch_config,
+                    {
+                        'output_dir': output_dir_launch_config,
+                        'lidar_topics': lidar_topics_launch_config
+                    }
+                ],
                 output="screen",
             ),
         ]
